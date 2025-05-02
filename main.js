e33ce556edf9d1c0de1e40750d9d00ddc085b0a9
Erika Zanin <erikaczbu@gmail.com>
2025-05-02 11:22:42 -0300

    Add js grafico

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+
+
 function go() {
     var nomeUser = document.querySelector('#nameU').value;
     var numberEntrada = parseFloat(document.querySelector('#numberEntrada').value);
     var numberSaida = parseFloat(document.querySelector('#numberSaida').value);
 
-    const dados = {
-        "Name user": nomeUser,
-        "Value entry": `${numberEntrada}`,
-        "Value exit": `${numberSaida}`
+    let somaGastos = (numberSaida / numberEntrada) * 100;
+
+    const dadosCadastro = {
+        NameUser: nomeUser,
+        ValueEntry: numberEntrada,
+        ValueExit: numberSaida,
+        SomaGastos: somaGastos
     };
 
-    console.log(JSON.stringify(dados, null, 2));
+    console.log(JSON.stringify(dadosCadastro, null, 2));
 
     /*transicao pag*/
 
@@ -37,74 +42,115 @@ function go() {
     }, tempoInicial + tempoEntreFades);
 
     setTimeout(() => {
+        localStorage.setItem('dadosCadastro', JSON.stringify(dadosCadastro));
         window.location.href = "./index.html";
     }, tempoInicial + tempoEntreFades + tempoTransicaoFinal);
 
-
-    let somaGastos = (numberSaida / numberEntrada) * 100;
-    var circle = document.querySelector('#circleProgress');
-
-    console.log(`Valor de entrada: ${somaGastos.toFixed(2)}%`);
-    document.querySelector('.number').innerHTML = somaGastos.toFixed(2) + '%';
-
-    circle.style.strokeDashoffset = 440 - (440 * somaGastos) / 100;
-    document.querySelector('.valueEntrada').innerHTML = numberEntrada;
-    document.querySelector('.valueSaida').innerHTML = numberSaida;
-
-
-    /*Date*/
-    const currentDate = new Date();
-
-    const currentDayOfMonth = currentDate.getDate();
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
-    let currentHours = currentDate.getHours();
-    const currentMinutes = currentDate.getMinutes();
-
-    const dateString = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
-
-    const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    const dateWithFullMonthName = monthNames[currentMonth];
-    const ordinalDateWithDayOfWeek = daysOfWeek[currentDate.getDay()];
-
-    // Converter hora para 12h com AM/PM
-    let period = "AM";
-    if (currentHours >= 12) {
-        period = "PM";
-    }
-    if (currentHours === 0) {
-        currentHours = 12;
-    } else if (currentHours > 12) {
-        currentHours -= 12;
-    }
-
-    const formattedMinutes = currentMinutes < 10 ? "0" + currentMinutes : currentMinutes;
-
-    const dateDado = {
-        "Hours": currentHours,
-        "Minutes": formattedMinutes,
-        "Period": period,
-        "Day": currentDayOfMonth,
-        "Month": currentMonth,
-        "Year": currentYear,
-        "Date": dateString,
-        "Name month": dateWithFullMonthName,
-        "Name week": ordinalDateWithDayOfWeek
-    };
-    console.log(dateDado);
-
-    const dateFormat = dateWithFullMonthName + ' ' + currentDayOfMonth + ', ' + ordinalDateWithDayOfWeek;
-    const hoursFormat = currentHours + ':' + formattedMinutes + ' ' + period;
-    console.log(dateFormat + ' ' + hoursFormat);
-
-    document.querySelector('.date').innerHTML = dateFormat;
-    document.querySelector('.hours').innerHTML = hoursFormat;
+    console.log(dadosCadastro)
 }
+
+
+/*Date*/
+const currentDate = new Date();
+
+const currentDayOfMonth = currentDate.getDate();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+let currentHours = currentDate.getHours();
+const currentMinutes = currentDate.getMinutes();
+
+const dateString = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
+
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const dateWithFullMonthName = monthNames[currentMonth];
+const ordinalDateWithDayOfWeek = daysOfWeek[currentDate.getDay()];
+
+// Converter hora para 12h com AM/PM
+let period = "AM";
+if (currentHours >= 12) {
+    period = "PM";
+}
+if (currentHours === 0) {
+    currentHours = 12;
+} else if (currentHours > 12) {
+    currentHours -= 12;
+}
+
+const formattedMinutes = currentMinutes < 10 ? "0" + currentMinutes : currentMinutes;
+
+const dateDado = {
+    "Hours": currentHours,
+    "Minutes": formattedMinutes,
+    "Period": period,
+    "Day": currentDayOfMonth,
+    "Month": currentMonth,
+    "Year": currentYear,
+    "Date": dateString,
+    "Name month": dateWithFullMonthName,
+    "Name week": ordinalDateWithDayOfWeek
+};
+console.log(dateDado);
+
+const dateFormat = dateWithFullMonthName + ' ' + currentDayOfMonth + ', ' + ordinalDateWithDayOfWeek;
+const hoursFormat = currentHours + ':' + formattedMinutes + ' ' + period;
+console.log(dateFormat + ' ' + hoursFormat);
+
+document.querySelector('.date').innerHTML = dateFormat;
+document.querySelector('.hours').innerHTML = hoursFormat;
+
+
+/*recuperar dados cadastrais*/
+
+const dadosSalvos = JSON.parse(localStorage.getItem('dadosCadastro'));
+var circle = document.querySelector('#circleProgress');
+
+if (dadosSalvos) {
+    const nomeUser = dadosSalvos["Name user"];
+    const numberEntrada = parseFloat(dadosSalvos["ValueEntry"]);
+    const numberSaida = parseFloat(dadosSalvos["ValueExit"]);
+    const numberSomaGastos = parseFloat(dadosSalvos["SomaGastos"]);
+    
+    if (numberEntrada > numberSaida) {
+        circle.style.strokeDashoffset = 440 - (440 * numberSomaGastos) / 100;
+    } else {
+        circle.style.strokeDashoffset = 0
+    }
+
+    document.querySelector('.valueEntrada').innerText = numberEntrada;
+    document.querySelector('.valueSaida').innerText = numberSaida;
+    document.querySelector('.number').innerText = `${numberSomaGastos} %`;
+
+    // Exemplo se quiser mostrar no console
+    console.log(`Bem-vindo, ${nomeUser}`);
+}
+
+
+
+
+/*grafico saude*/
+
+/*Agua saude*/
+const dadoAgua = document.querySelector('.aguaValue');
+const metaAgua = 4;
+const valorPorcentagemTotal = 90;
+
+const valorConsumidoAgua = 3;
+const porcentagemConsumida = (valorConsumidoAgua / metaAgua); // entre 0 e 1
+const alturaCalculada = valorPorcentagemTotal * Math.min(porcentagemConsumida, 1); // garante no máximo 100%
+
+console.log(`Porcentagem de consumo: ${porcentagemConsumida * 100}%`);
+console.log(`Altura a aplicar: ${alturaCalculada}px`);
+
+dadoAgua.style.height = `${alturaCalculada}px`;
+
+/*Sono saude*/
+
+console.log('Gráfico saúde carregado');
 
 
 
